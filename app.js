da38fa89c4a5634616410224a1242802373c16cd
angelw22 <23022872+angelw22@users.noreply.github.com>
2023-12-13 12:15:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,18 +6,22 @@ var logger = require('morgan');
 const WebSocket = require("ws");
 const WebSocketServer = require("ws").Server;
 
+const PORT = process.env.PORT || 9876;
+const myServer = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 let clientArr = {}; 
 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,8 +29,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,9 +49,8 @@ app.use(function(err, req, res, next) {
 });
 
 
-app.use("/",express.static(path.resolve(__dirname, "../client")))
+// app.use("/",express.static(path.resolve(__dirname, "../client")))
 
-const myServer = app.listen(9876)       // regular http server using node express which serves your webpage
 
 const wsServer = new WebSocketServer({ noServer: true });// a websocket server
 
@@ -71,6 +74,7 @@ wsServer.on("connection", function(ws) {    // what should a websocket do on con
 
         if (JSON.parse(msg).type === "update") {
           let coords = JSON.parse(msg).data;
+          console.log("check client", clientArr[ws.id])
           clientArr[ws.id].x = coords.x;
           clientArr[ws.id].y = coords.y;
         }
